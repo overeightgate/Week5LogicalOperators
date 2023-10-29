7ccdd3039c2434a1180e39ea90b537507e208964
Markus <Markus@20111759-D>
2023-10-29 19:24:24 +0200

    Add project files.

---
+++ OROperator/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math results:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry results");
+chemistry = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology results");
+biology = Int32.Parse(Console.ReadLine());
+
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+
+else
+{
+    Console.WriteLine("Your application hasn't been apporved");
